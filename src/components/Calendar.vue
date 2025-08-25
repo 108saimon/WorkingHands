d31447874b3e5c843a20867586057d538f781868
aayakovenko <aayakovenko@>
2025-08-25 10:15:36 +0300

    add Calendar component and props

---
+++ src/components/Calendar.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineOptions, defineProps } from 'vue';
+
+defineOptions({
+  name: 'Calendar'
+});
+
+defineProps({
+  initialDate: {
+    type: String,
+    default: null
+  },
+  language: {
+    type: String,
+    default: 'ru'
+  }
+})
+
+
+</script>
